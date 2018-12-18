86a98cc1017a17128d289feca1c3863e60da041b
Gou-Opa <monneymrt1998@gmail.com>
2018-12-19 00:45:49 +0700

    change all yum to apt

--- deployscript/install.sh
+++ deployscript/install.sh
@@ -2,11 +2,11 @@
 set -e
 
 # update instance
-yum -y update
+apt-get update
 
 # add nodejs to yum
 curl --silent --location https://rpm.nodesource.com/setup_10.x | sudo bash -
-yum -y install nodejs #default-jre ImageMagick
+apt install nodejs -y #default-jre ImageMagick
 
 # install pm2 module globaly
 npm install -g serve
